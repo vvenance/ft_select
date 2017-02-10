f81dcb5573373c7db4ff4ca015aa54e70053c860
Mac de Sonny <Spetit@macbook-pro-de-mac.home>
2017-02-10 11:01:24 +0100

    add usage

--- src/main.c
+++ src/main.c
@@ -8,6 +8,8 @@ int main(int ac, char **av)
 	struct termios	config;
 
 	list = NULL;
+	if (ac < 2)
+		ft_putendl_fd("Usage: ./ft_select [arglist ...].", 2);
 	if (av[1] && getenv("TERM") != NULL && (init_termcaps(&term, &config)))
 	{
 		ft_putendl("1");
